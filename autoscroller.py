ea0f53408c512e7e2958c8913bd4b4ce96656f47
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-01-18 23:40:55 +0530

    Update

--- autoscroller.py
+++ autoscroller.py
@@ -1,14 +1,15 @@
-import pyautogui,time
+import pyautogui, time
 
 def main():
 	screenWidth, screenHeight = pyautogui.size()
+
 	pyautogui.moveTo(screenWidth/2, screenHeight/2)
 	pyautogui.moveTo(200, 500)
 
 	pyautogui.click()
+
 	while True:
-		pyautogui.scroll(-50) #scrolls down
-		time.sleep(5)       #sleep for 10 seconds
-	
+		pyautogui.scroll(-50) # scrolls down
+		time.sleep(5)         # sleep for 5 seconds
 
 main() # Call of the main function
